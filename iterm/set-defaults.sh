15b344344907e6596fe681107d58fc5346cfaec0
Wan Liuyang <tsfdye@gmail.com>
2018-08-17 15:17:34 +0800

    Set up iterm2 pref sync from command line

---
+++ iterm/set-defaults.sh
@@ -0,0 +1,7 @@
+# Specify the preferences directory
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.dotfiles/iterm/"
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+
+# Reset the preferences cache
+killall cfprefsd
