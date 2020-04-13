10509488a82aa1a1d44a61b53c3e73db007f14b2
Liuyang Wan <tsfdye@gmail.com>
2020-04-13 09:04:55 +0800

    Update default iterm pref path

--- iterm/set-defaults.sh
+++ iterm/set-defaults.sh
@@ -1,5 +1,5 @@
 # Specify the preferences directory
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.dotfiles/iterm/"
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/Dropbox/iTerm/"
 # Tell iTerm2 to use the custom preferences in the directory
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
 
