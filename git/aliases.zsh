ca0b466f8c1ef021e7dd6446434f3a55acbd3132
Wan Liuyang <tsfdye@gmail.com>
2021-04-16 09:11:12 +0800

    Move lg to alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -8,4 +8,6 @@ alias gb='gh repo view -w'
 alias pc='gh pr create -w'  # create pull request on web
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit'
+alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
 
