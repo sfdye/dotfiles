1a531ef950a943c1fbaa6fa073607f8c88353d1d
Liuyang Wan <tsfdye@gmail.com>
2024-04-17 10:56:29 +0100

    Add git alias gbdn

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -10,4 +10,13 @@ alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit'
 alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
+function gbdn() {
+  default_branch=$(git config remote.origin.defaultbranch)
+  if [ $? -ne 0 ]; then
+    default_branch=$(gh repo view --json defaultBranchRef -q .defaultBranchRef.name)
+    echo "setting default branch to $default_branch"
+    git config --local remote.origin.defaultbranch $default_branch
+  fi
 
+  git checkout $default_branch && git pull && git bclean $default_branch;
+}
